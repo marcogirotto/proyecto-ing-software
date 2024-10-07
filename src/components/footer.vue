ba5bd0cf483d1826265404fbf9f3beb937d0b7f3
marcogirotto <git@stackblitz.com>
2024-10-07 16:14:25 -0400

    Initial commit

---
+++ src/components/footer.vue
@@ -0,0 +1,28 @@
+<template>
+<footer class="Footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>My Vue App</strong> by
+      <p>empresa</p>. © 2024
+    </p>
+  </div>
+</footer>
+</template>
+
+<script>
+export default {
+name: 'Footer',
+};
+</script>
+
+<style scoped>
+.footer {
+background-color: yellow;
+padding: 1rem;
+color: white;
+position: fixed;
+bottom: 0;
+width: 100%;
+}
+
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <a class="encabezadoItem" href="#">
+        <img src="https://vuejs.org/images/logo.png" alt="Vue logo" />
+      </a>
+    </div>
+
+    <div class="encabezadoMenu">
+      <div class="encabezadoInicio">
+        <a class="encabezadoItem" href="/">Incio</a>
+        <a class="encabezadoItem" href="/about">Top juegos</a>
+        <a class="encabezadoItem" href="/contact">Review</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+};
+</script>
+
+<style scoped>
+.navbar {
+  background-color: #42b983;
+  padding: 1rem;
+}
+
+.encabezadoItem {
+  color: white;
+  text-decoration: none;
+  padding: 0.5rem 1rem;
+}
+
+.encabezadoItem:hover {
+  background-color: #35495e;
+}
+</style>
